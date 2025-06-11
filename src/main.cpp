d94ab79a1ea3fe2bf95d5ac96f6645276b002e91
Hendrik Siemund <hendrik.smd@proton.me>
2025-06-12 00:52:03 +0200

    Added simple REPL

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+
+#include "conkey/lexer/lexer.hpp"
+
+const std::string PROMPT = ">>";
 
 int main() {
-    std::cout << "tttt" << std::endl;
+    std::string line;
+
+    std::cout << PROMPT;
+    while(std::getline(std::cin, line)) {
+        std::istringstream input(line);
+        Conkey::Lexer::Lexer lexer(input, "cin");
+        while (!lexer.noMoreTokens()) {
+            std::cout << lexer.tryNextToken() << std::endl;
+        }
+        std::cout << PROMPT;
+    }
 }
